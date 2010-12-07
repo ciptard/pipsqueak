c9fc3adfc7fda68af548c2618763cb427135ddf3
Mark Perkins <mark@medium.io>
2010-12-07 23:29:09 +0000

    Remove unused globistr helper.

--- app/classes/helpers.php
+++ app/classes/helpers.php
@@ -12,48 +12,18 @@ class Helpers {
         return FALSE;
     }
     
-    /*
-     * http://www.php.net/manual/en/function.glob.php#101043
-     */
-    public static function globistr($string = '', $mbEncoding = ''){
-        $return = "";
-        if($mbEncoding !== ''){
-            $string = mb_convert_case($string,MB_CASE_LOWER,$mbEncoding);
-        }
-        else{
-            $string = strtolower($string);
-        }
-        $mystrlen = strlen($string);
-        for($i=0;$i<$mystrlen;$i++){
-            if($mbEncoding !== ''){
-                $myChar = mb_substr($string,$i,1,$mbEncoding);
-                $myUpperChar = mb_convert_case($myChar,MB_CASE_UPPER,$mbEncoding);
-            }else{
-                $myChar = substr($string,$i,1);
-                $myUpperChar = strtoupper($myChar);
-            }
-            if($myUpperChar !== $myChar){
-                $return .= '['.$myChar.$myUpperChar.']'; 
-            }else{
-                $return .= $myChar;
-            }
-        }
-        return $return;
-    }
-
-    
     // http://www.php.net/manual/en/function.file-put-contents.php#84180
     function file_force_contents($dir, $contents)
     {
-            $parts = explode('/', $dir);
-            $file = array_pop($parts);
-            $dir = '';
-            foreach($parts as $part)
-            {
-                if(!is_dir($dir .= "/$part")) mkdir($dir);
-            }
-            file_put_contents("$dir/$file", $contents);
+        $parts = explode('/', $dir);
+        $file = array_pop($parts);
+        $dir = '';
+        foreach($parts as $part)
+        {
+            if(!is_dir($dir .= "/$part")) mkdir($dir);
         }
+        file_put_contents("$dir/$file", $contents);
+    }
     
 }
 
